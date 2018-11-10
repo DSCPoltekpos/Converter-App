0e9fa53a3b62d9881e31cecb801d5829c767b7ec
Imron Sumadireja <imronsumadireza@gmail.com>
2018-11-10 21:34:58 +0700

    done

---
+++ app/src/main/java/com/sumadireja/temperature/Main2Activity.java
@@ -0,0 +1,25 @@
+package com.sumadireja.temperature;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    TextView hasilConvert, satuanPilihan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        hasilConvert = findViewById(R.id.hasil_convert);
+        satuanPilihan = findViewById(R.id.satuan_pilihan);
+
+        Intent intent = getIntent();
+
+        hasilConvert.setText(intent.getStringExtra("HASIL_CONVERT"));
+        satuanPilihan.setText(intent.getStringExtra("SATUAN_PILIHAN"));
+    }
+}
